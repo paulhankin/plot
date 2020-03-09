8c636b1472e079dd61f2956eea0dbf3721ec7594
Paul Hankin <paul.hankin@pobox.com>
2020-03-09 22:22:37 +0100

    factor more of the cmd code

--- cmd/svgtogcode/main.go
+++ cmd/svgtogcode/main.go
@@ -13,12 +13,10 @@ import (
 	"github.com/rustyoz/svg"
 )
 
-type flagSizeValue struct {
-	X, Y float64
-}
+type flagSizeValue paths.Vec2
 
 func (fs *flagSizeValue) String() string {
-	return fmt.Sprintf("%.2f,%.2f", fs.X, fs.Y)
+	return fmt.Sprintf("%.2f,%.2f", fs[0], fs[1])
 }
 
 func parseSizePart(s string) (float64, error) {
@@ -33,16 +31,16 @@ func (fs *flagSizeValue) Set(s string) error {
 	var err error
 	parts := strings.Split(s, ",")
 	if len(parts) == 1 {
-		fs.X, err = parseSizePart(parts[0])
+		fs[0], err = parseSizePart(parts[0])
 		return err
 	}
 	if len(parts) > 2 {
 		return fmt.Errorf("can't parse %q as size", s)
 	}
-	if fs.X, err = parseSizePart(parts[0]); err != nil {
+	if fs[0], err = parseSizePart(parts[0]); err != nil {
 		return err
 	}
-	if fs.Y, err = parseSizePart(parts[1]); err != nil {
+	if fs[1], err = parseSizePart(parts[1]); err != nil {
 		return err
 	}
 	return nil
@@ -53,9 +51,9 @@ var (
 	flagIn  string
 	flagOut string
 
-	flagDelta     flagSizeValue
-	flagSize      flagSizeValue
-	flagPaperSize flagSizeValue
+	flagDelta     paths.Vec2
+	flagSize      paths.Vec2
+	flagPaperSize paths.Vec2
 	flagCenter    bool
 	flagPenUp     int
 	flagFeedRate  int
@@ -64,9 +62,9 @@ var (
 func init() {
 	flag.StringVar(&flagIn, "in", "", "svg input file")
 	flag.StringVar(&flagOut, "out", "out.gcode", "gcode output file")
-	flag.Var(&flagDelta, "offset", "displacement of 0,0 from pen origin")
-	flag.Var(&flagSize, "size", "target size of image (mm)")
-	flag.Var(&flagPaperSize, "paper", "target size of paper (mm)")
+	flag.Var((*flagSizeValue)(&flagDelta), "offset", "displacement of 0,0 from pen origin")
+	flag.Var((*flagSizeValue)(&flagSize), "size", "target size of image (mm)")
+	flag.Var((*flagSizeValue)(&flagPaperSize), "paper", "target size of paper (mm)")
 	flag.BoolVar(&flagCenter, "center", false, "if set, center image on paper")
 	flag.IntVar(&flagPenUp, "penup", 40, "how much to lift pen when moving")
 	flag.IntVar(&flagFeedRate, "feed", 800, "feed rate when drawing (mm/min)")
@@ -79,6 +77,46 @@ func parseSVG(name string) (*svg.Svg, error) {
 	}
 	defer f.Close()
 	return svg.ParseSvgFromReader(f, "", 1.0)
+}
+
+func adjustSize(sz, ps, delta paths.Vec2, center bool, b paths.Bounds) (paths.Bounds, error) {
+	ow := b.Max[0] - b.Min[0]
+	oh := b.Max[1] - b.Min[1]
+	if sz[0] == 0 && sz[1] == 0 {
+		sz[0] = ow
+		sz[1] = oh
+	} else if sz[1] == 0 {
+		sz[1] = sz[1] * oh / ow
+	} else if sz[0] == 0 {
+		sz[0] = sz[0] * ow / oh
+	}
+
+	if !(math.Abs(sz[0]/sz[1]-ow/oh) < 1e-3) {
+		return paths.Bounds{}, fmt.Errorf("target image size %s not compatible with image size %g,%g", &sz, ow, oh)
+	}
+
+	if ps[0] != 0 || ps[1] != 0 {
+		if ps[0] == 0 || ps[1] == 0 {
+			return paths.Bounds{}, fmt.Errorf("paper size %g,%g doesn't make sense", ps[0], ps[1])
+		}
+
+		if sz[0] > ps[0] || sz[1] > ps[1] {
+			return paths.Bounds{}, fmt.Errorf("paper size %g,%g is smaller than image %g,%g", ps[0], ps[1], sz[0], sz[1])
+		}
+	}
+
+	if center {
+		if ps[0] == 0 {
+			return paths.Bounds{}, fmt.Errorf("must set -papersize to use -center")
+		}
+		delta[0] += (ps[0] - sz[0]) / 2
+		delta[1] += (ps[1] - sz[1]) / 2
+	}
+
+	return paths.Bounds{
+		Min: paths.Vec2{delta[0], delta[1]},
+		Max: paths.Vec2{sz[0] + delta[0], sz[1] + delta[1]},
+	}, nil
 }
 
 func main() {
@@ -101,43 +139,12 @@ func main() {
 		return paths.FromSVG(f)
 	}()
 
-	ow := ps.Bounds.Max[0] - ps.Bounds.Min[0]
-	oh := ps.Bounds.Max[1] - ps.Bounds.Min[1]
-	if flagSize.X == 0 && flagSize.Y == 0 {
-		flagSize.X = ow
-		flagSize.Y = oh
-	} else if flagSize.Y == 0 {
-		flagSize.Y = flagSize.Y * oh / ow
-	} else if flagSize.X == 0 {
-		flagSize.X = flagSize.X * ow / oh
+	bounds, err := adjustSize(flagSize, flagPaperSize, flagDelta, flagCenter, ps.Bounds)
+	if err != nil {
+		fail("%s", err)
 	}
 
-	if !(math.Abs(flagSize.X/flagSize.Y-ow/oh) < 1e-3) {
-		fail("target image size %s not compatible with image size %g,%g", &flagSize, ow, oh)
-	}
-
-	if flagPaperSize.X != 0 || flagPaperSize.Y != 0 {
-		if flagPaperSize.X == 0 || flagPaperSize.Y == 0 {
-			fail("paper size %g,%g doesn't make sense", flagPaperSize.X, flagPaperSize.Y)
-		}
-
-		if flagSize.X > flagPaperSize.X || flagSize.Y > flagPaperSize.Y {
-			fail("paper size %g,%g is smaller than image %g,%g", flagPaperSize.X, flagPaperSize.Y, flagSize.X, flagSize.Y)
-		}
-
-	}
-	if flagCenter {
-		if flagPaperSize.X == 0 {
-			fail("must set -papersize to use -center")
-		}
-		flagDelta.X += (flagPaperSize.X - flagSize.X) / 2
-		flagDelta.Y += (flagPaperSize.Y - flagSize.Y) / 2
-	}
-
-	ps.Transform(paths.Bounds{
-		Min: paths.Vec2{flagDelta.X, flagDelta.Y},
-		Max: paths.Vec2{flagSize.X + flagDelta.X, flagSize.Y + flagDelta.Y},
-	})
+	ps.Transform(bounds)
 
 	gcodeOut, err := os.Create(flagOut)
 	if err != nil {
